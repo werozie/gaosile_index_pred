caf8f40f9c50a492410e4a9514010db123964f0e
werozie <152220843+werozie@users.noreply.github.com>
2023-11-27 22:06:45 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# Projekt - predykcja cen paliw
+
+
+#test
+print("Hello world")
+
+
+# 1. załadowanie danych
+
+
+# 2. wizualizacja dostepnych danych (rozkład, wykres liniowy, mapa korelacji)
+
+
+# 3. SARIMA
+
+
+# 4. LSTM
+
+
+# 5. Ocena modeli
+
